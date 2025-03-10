e8a14d31b87829ba302e6912ce9310e2e875bd08
Wesley Bliss <wesley.bliss@gmail.com>
2025-03-10 10:16:22 -0500

    Cleanup

--- lib/ui/screens/home/widgets/current_exchange_rates_info.dart
+++ lib/ui/screens/home/widgets/current_exchange_rates_info.dart
@@ -21,33 +21,38 @@ class CurrentExchangeRatesInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     // @todo make this use the currently selected currency
     final nf = NumberFormat.currency(locale: 'en_US', symbol: '');
+    final Widget child;
 
     if (showCurrencyRate == "all") {
-      return Text(selectedCurrencies.map((e) => '${e.symbol}: ${nf.format(e.rate)}').join('\n'));
+      child = Text(selectedCurrencies.map((e) => '${e.symbol}: ${nf.format(e.rate)}').join('\n'), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 17, fontFamily: 'monospace', color: Theme.of(context).colorScheme.surfaceTint));
     } else {
       final currentExchangeRate =
           focusedCurrency == null ? '' : '\$1 = ${nf.format(focusedCurrency?.rate)} $focusedCurrencyInputSymbol';
 
-      return ConditionallyVisible(
-        isVisible: showCurrencyRate == "selected",
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "EXCHANGE RATE",
-              textAlign: TextAlign.center,
-              style: Theme.of(
-                context,
-              ).textTheme.labelLarge?.copyWith(color: Theme.of(context).colorScheme.onSurface.withAlpha(125)),
-            ),
-            Text(
-              currentExchangeRate,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 17, fontFamily: 'monospace', color: Theme.of(context).colorScheme.surfaceTint),
-            ),
-          ],
-        ),
+      child = Text(
+        currentExchangeRate,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 17, fontFamily: 'monospace', color: Theme.of(context).colorScheme.surfaceTint),
       );
     }
+
+    return ConditionallyVisible(
+      isVisible: showCurrencyRate == "selected" || showCurrencyRate == "all",
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "EXCHANGE RATE",
+            textAlign: TextAlign.center,
+            style: Theme.of(
+              context,
+            ).textTheme.labelLarge?.copyWith(color: Theme.of(context).colorScheme.onSurface.withAlpha(125)),
+          ),
+          SizedBox(height: 10),
+          child,
+        ],
+      ),
+    );
   }
 }
