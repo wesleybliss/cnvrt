56725c275a01e73686a0c1330284a85aa7a6d9ad
Wesley Bliss <wesley.bliss@gmail.com>
2025-10-15 15:38:13 -0500

    Added beta testers group to release script

--- create-release.sh
+++ create-release.sh
@@ -10,6 +10,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     flutter build apk --release && \
         firebase appdistribution:distribute \
             build/app/outputs/flutter-apk/app-release.apk \
-            --app '1:1021931476908:android:0ef35750044f283fef4c09'
+            --app '1:1021931476908:android:0ef35750044f283fef4c09' \
+            --groups 'alpha-testers'
     
 fi
