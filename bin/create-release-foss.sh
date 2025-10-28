94e29b27df2750cca4729bf820b5ddaf57cebe5a
Wesley Bliss <wesley.bliss@gmail.com>
2025-10-28 16:25:38 +0100

    Added FOSS build script for convenience

--- bin/create-release-foss.sh
+++ bin/create-release-foss.sh
@@ -7,7 +7,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     
     echo "Creating production release"
     
-    flutter build apk --release --flavor standard && \
+    flutter build apk --release --flavor foss --dart-define=FOSS_BUILD=true && \
         firebase appdistribution:distribute \
             build/app/outputs/flutter-apk/app-release.apk \
             --app '1:1021931476908:android:0ef35750044f283fef4c09' \
