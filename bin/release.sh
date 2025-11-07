55fcce7592e9b33928b530395b6319c60344bf4c
Wesley Bliss <wesley.bliss@gmail.com>
2025-11-07 16:31:24 +0100

    Updated screenshots and fixed paths on release script

--- bin/release.sh
+++ bin/release.sh
@@ -9,7 +9,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     
     flutter build apk --release --flavor standard && \
         firebase appdistribution:distribute \
-            build/app/outputs/flutter-apk/app-release.apk \
+            build/app/outputs/flutter-apk/app-standard-release.apk \
             --app '1:1021931476908:android:0ef35750044f283fef4c09' \
             --groups 'alpha-testers'
     
